ee94fa2ac589585ea3137b9c0efccf4a878a9fbb
이건형 <102940970+zeetee1235@users.noreply.github.com>
2025-02-08 03:09:41 +0900

    Add files via upload

---
+++ resources.py
@@ -0,0 +1,5 @@
+import torch
+
+print("GPU 사용 가능 여부:", torch.cuda.is_available())
+print("사용 가능한 GPU 개수:", torch.cuda.device_count())
+print("GPU 이름:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "None")
